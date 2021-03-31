9ba3ddaf687fd053696410a9b697c4e36f9f4181
df11047 <dfu2003@gmail.com>
2021-03-31 13:50:39 -0400

    created climb subsystem

---
+++ src/main/java/frc/robot/climb/ClimbSubsystem.java
@@ -0,0 +1,12 @@
+package frc.robot.climb;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimbSubsystem extends SubsystemBase {
+    
+
+
+
+    
+}
