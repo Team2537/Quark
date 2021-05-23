982b76ac20752f7973c47158fd351583cf13118c
dnag257 <80492599+dnag257@users.noreply.github.com>
2021-05-23 18:45:15 -0400

    added lasers and methods

--- src/main/java/frc/robot/laser/LaserSubsystem.java
+++ src/main/java/frc/robot/laser/LaserSubsystem.java
@@ -1,9 +1,16 @@
 package frc.robot.laser;
 
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LaserSubsystem extends SubsystemBase {
-    
+    private DigitalOutput laser = new DigitalOutput(0);
+    LaserSubsystem(){}
 
-    
+    public void laserOn() {
+        laser.set(true);
+    }
+    public void laserOff() {
+        laser.set(false);
+    }
 }
